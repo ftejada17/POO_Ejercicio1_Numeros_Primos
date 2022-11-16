3b335228c9a277573c1bd345b76654833948b805
pequenno17 <fjtejada17@gmail.com>
2022-11-16 10:54:53 +0100

    Primer versión del ejercicio de los numeros primos

---
+++ NumerosPrimos.java
@@ -0,0 +1,63 @@
+public class NumerosPrimos 
+{
+    private int suma;
+    public void sumPrimesInsideFirstNumbers()
+    {
+        System.out.println("SUMA DE LOS NUMEROS PRIMOS EXISTENTES ENTRE EL 0~50:");        
+
+        for(int i = 2; i < 51; i++)
+        {            
+            if(isPrime(i))
+            {
+                System.out.println("- " + i);
+                suma+=i;
+            }
+        }
+
+        System.out.println("- La suma de todos los numeros es: " + suma);
+    }
+    public void sumFirstFifty()
+    {
+        System.out.println("SUMA DE LOS 50 PRIMEROS NUMEROS PRIMOS EXISTENTES:");
+        int contador = 0;
+        int number = 2;      //Cero y Uno no son primos asi que no se comprueban
+        do
+        {
+            if(isPrime(number))
+            {
+                contador++;
+                System.out.println("- " + number);
+                suma += number;
+            }
+            number++;
+        }
+        while(contador < 50);
+
+        System.out.println("- La suma de todos los numeros es: " + suma);
+    }
+
+    
+
+    private boolean isPrime(int n)
+    {
+        for(int i = 2; i < n; i++)
+        {
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+    private void resetSuma()
+    {
+        suma = 0;
+    }
+
+
+
+    public static void main(String[]args)
+    {
+        NumerosPrimos npc = new NumerosPrimos();
+        npc.sumPrimesInsideFirstNumbers();
+        npc.resetSuma();
+        npc.sumFirstFifty();
+    }
+}
